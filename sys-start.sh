59ec2b6131a0eba228dd9015d2589b85e90b964a
Insert-Generic-Name <253096105+Insert-Generic-Name@users.noreply.github.com>
2026-07-20 23:25:53 +0200

    Add Setup

--- sys-start.sh
+++ sys-start.sh
@@ -3,11 +3,14 @@ set -euo pipefail
 
 # This script bootstraps the system itself and fakes the loadbalancer. Its not related to the k8s deployment itself, its just a convenient "off-the-books" script ( mainly because Gemini vibe coded it )
 
+# Setup: Run this command ONCE, it allows the script to run sudo-less.
+#:~$ sudo setcap 'cap_net_bind_service=+ep' $(readlink -f $(which kubectl))
+
 # Variables, set this manually
 VM_NAME="talslop"
 ENVOY_SVC="envoy-network-system-envoy-gateway-b7e4c8a5"
 LISTENER_IP=$(tailscale ip -4)
-
+FORWARDED_PORTS=(80 )
 
 # VM bootstrap
 if virsh -c qemu:///system domstate "${VM_NAME}" 2>/dev/null | grep -q "running"; then
@@ -27,7 +30,7 @@ echo " Setting up fake loadbalancer. To stop it, press Ctrl+C twice"
 echo "##############################################################"
 while true; do
     # The '|| true' keeps 'set -e' from exiting the script when kubectl drops
-    kubectl port-forward -n network-system "svc/${ENVOY_SVC}" 80 443 --address "${LISTENER_IP}" || true
+    kubectl port-forward -n network-system "svc/${ENVOY_SVC}" "${FORWARDED_PORTS[@]}" --address "${LISTENER_IP}" || true
 
     echo "Load Balancer dropped, reconnecting in 2 seconds..."
     sleep 2
