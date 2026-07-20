fd30bd77dc71434ec31f3ffa332622060f18918d
Insert-Generic-Name <253096105+Insert-Generic-Name@users.noreply.github.com>
2026-07-20 22:47:50 +0200

    Update script

--- sys-start.sh
+++ sys-start.sh
@@ -3,15 +3,23 @@ set -euo pipefail
 
 # This script bootstraps the system itself and fakes the loadbalancer. Its not related to the k8s deployment itself, its just a convenience messed up script
 
+# Variables, set this manually
 VM_NAME="talslop"
-ENVOY_SVC=""
-# Start VM on boot
-virsh -c qemu:///system start ${VM_NAME}
-echo "#########################################"
-echo "Setting up the VM"
-echo "#########################################"
-sleep 30
-echo "#########################################"
-echo "VM is running, setting up fake loadbalancer"
-echo "#########################################"
-kubectl port-forward -n network-system svc/${ENVOY_SVC} 12000:80 --address 0.0.0.0
+ENVOY_SVC="envoy-network-system-envoy-gateway-b7e4c8a5"
+#
+
+if virsh -c qemu:///system domstate "${VM_NAME}" 2>/dev/null | grep -q "running"; then
+    echo "######################################################"
+    echo "VM '${VM_NAME}' is already running."
+    echo "######################################################"
+else
+    virsh -c qemu:///system start "${VM_NAME}"
+    echo "######################################################"
+    echo "Setting up VM '${VM_NAME}'"
+    echo "######################################################"
+    sleep 30
+fi
+echo "##############################################################"
+echo " Setting up fake loadbalancer. To stop it, close the terminal"
+echo "##############################################################"
+while true; do kubectl port-forward -n network-system svc/${ENVOY_SVC} 12000:80 --address 0.0.0.0; done
